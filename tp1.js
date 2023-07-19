8d39f50376e351d551fbaf48ca242ab0bd930844
wejdene-benamor <benamorwejdene53@gmail.com>
2023-07-19 15:02:08 +0100

    solution

--- tp1.js
+++ tp1.js
@@ -3,3 +3,22 @@
  * @param {Array<any>} array
  * @return {any}
  */
+function midElement(t) {
+  var mid = "";
+  if (t.length % 2 == 0) {
+    console.log(
+      t[t.length / 2] +
+        "  and  " +
+        t[t.length / 2 - 1] +
+        "  both are middle elment"
+    );
+    return (
+      t[t.length / 2] + "and" + t[t.length / 2 - 1] + "both are middle elment"
+    );
+  } else {
+    console.log(t[Math.trunc(t.length / 2)]);
+    return t[Math.trunc(t.length / 2)];
+  }
+}
+
+midElement(["a", "b", "n", "j", "k", "j"]);
