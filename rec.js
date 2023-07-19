3cda5e99613741fe80577346307211473562f28b
wejdene-benamor <benamorwejdene53@gmail.com>
2023-07-19 17:02:29 +0100

    rec2

--- rec.js
+++ rec.js
@@ -1,38 +1,50 @@
-// Using a for loop THEN a while loop THEN recursion
-//  Write a JavaScript function that will iterate from n to 0. For each iteration,
-//  it will check if the current number is odd or even, and display a message to the screen.
-//  iterateAndLog(3); =>
-//  Sample Output :
-//  "2 is even"
-//  "1 is odd"
-//  "0 is even"
+// // Using a for loop THEN a while loop THEN recursion
+// //  Write a JavaScript function that will iterate from n to 0. For each iteration,
+// //  it will check if the current number is odd or even, and display a message to the screen.
+// //  iterateAndLog(3); =>
+// //  Sample Output :
+// //  "2 is even"
+// //  "1 is odd"
+// //  "0 is even"
 
-function reverseIterateAndLogWithFor(n) {
-  // TODO: your work goes here
-}
+// function reverseIterateAndLogWithFor(n) {
+//   // TODO: your work goes here
+// }
 
-function reverseIterateAndLogWithWhile(n) {
-  // TODO: your work goes here
-}
+// function reverseIterateAndLogWithWhile(n) {
+//   // TODO: your work goes here
+// }
 
-function reverseIterateAndLogRecursively(n) {
-  // TODO: your work goes here
-}
-function JavaScript(n) {
-  for (i = n; i >= 0; i--) {
-    if (i % 2 == 0) console.log(i + " is even");
-    else console.log(i + " is odd");
+// function reverseIterateAndLogRecursively(n) {
+//   // TODO: your work goes here
+// }
+// function JavaScript(n) {
+//   for (i = n; i >= 0; i--) {
+//     if (i % 2 == 0) console.log(i + " is even");
+//     else console.log(i + " is odd");
+//   }
+// }
+// //var res = JavaScript(8);
+// //console.log(res);
+// function JavaScriptWHIlE(n) {
+//   var i = n;
+//   while (i >= 0) {
+//     if (i % 2 == 0) console.log(i + "is even");
+//     else console.log(i + "is odd");
+//     i--;
+//   }
+// }
+// var res = JavaScriptWHIlE(9);
+// console.log(res);
+function x(n) {
+  if (n === 0) {
+    console.log("n is even");
+  } else if (n % 2 === 0) {
+    console.log(n + " is even");
+    x(n - 1);
+  } else {
+    console.log(n + " is odd");
+    x(n - 1);
   }
 }
-//var res = JavaScript(8);
-//console.log(res);
-function JavaScriptWHIlE(n) {
-  var i = n;
-  while (i >= 0) {
-    if (i % 2 == 0) console.log(i + "is even");
-    else console.log(i + "is odd");
-    i--;
-  }
-}
-var res = JavaScriptWHIlE(9);
-console.log(res);
+x(9);
