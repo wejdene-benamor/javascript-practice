a842ed5439dc783e2bf79c36a3eb3bce43ef1ddd
MohamedAliFaidi <dalyyy3259@gmail.com>
2023-07-19 14:28:20 +0100

    first problem

---
+++ tp1.js
@@ -0,0 +1,5 @@
+/**
+ * 2-write a function called (midElement) that takes an array as an argument and returns the element that is located in the middle of that array.
+ * @param {Array<any>} array
+ * @return {any}
+ */
