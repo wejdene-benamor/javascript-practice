3d8ef79a8f0b154b462e245a4eba78c081285296
MohamedAliFaidi <dalyyy3259@gmail.com>
2023-07-19 15:45:01 +0100

    rec

---
+++ rec.js
@@ -0,0 +1,26 @@
+// Using a for loop THEN a while loop THEN recursion
+//  Write a JavaScript function that will iterate from n to 0. For each iteration,
+//  it will check if the current number is odd or even, and display a message to the screen.
+//  iterateAndLog(3); =>
+//  Sample Output :
+//  "2 is even"
+//  "1 is odd"
+//  "0 is even"
+
+function reverseIterateAndLogWithFor(n) {
+
+  // TODO: your work goes here
+
+}
+
+function reverseIterateAndLogWithWhile(n) {
+
+  // TODO: your work goes here
+
+}
+
+function reverseIterateAndLogRecursively(n) {
+
+  // TODO: your work goes here
+
+}
