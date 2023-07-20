fc4f5d5b5bd198eb8935604a65acc1326726e930
wejdene-benamor <benamorwejdene53@gmail.com>
2023-07-20 15:40:32 +0100

    bubble

--- bubble.js
+++ bubble.js
@@ -26,6 +26,56 @@
  *  Example usage:
  * bubbleSortTest([2, 1, 3]); // yields [1, 2, 3]
  */
-var bubbleSort = function (array) {
-// TODO: Your code here!
-};
+// //function tribulle(t) {
+//   e = true;
+//   n = t.length;
+//   do {
+//       e=false
+//     var x = 0;
+//     for (let i = 0; i >= n; i++) {
+//       if (t[i] > t[i + 1]) {
+//         x = t[i];
+//         t[i] = t[i + 1];
+//         t[i + 1] = x;
+//       }
+//     }
+//   } while (e === false && n == 1);
+//   n = n - 1;
+//   return t
+// }
+// var res = tribulle([4, 3, 1, 6, 0, 8, 5]);
+// console.log(res);
+function bulle(t) {
+  var aux = 0;
+  for (let i = 0; i <= t.length; i++) {
+    for (let j = 0; j <= t.length; j++) {
+      if (t[j] > t[j + 1]) {
+        aux = t[j];
+        t[j] = t[j + 1];
+        t[j + 1] = aux;
+      }
+    }
+  }
+  return t;
+}
+var res = bulle([0, 7, 5, 3, 99]);
+console.log(res);
+function recursiveBubbleSort(arr, n) {
+  // Base case: If the array has one or zero elements, it is already sorted
+  if (n <= 1) {
+    return;
+  }
+
+  // One pass through the array to move the largest element to its correct position
+  for (let i = 0; i < n - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      // Swap elements
+      var temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+    }
+  }
+
+  // Recur with one less element since the last element is already in its correct position
+  recursiveBubbleSort(arr, n - 1);
+}
